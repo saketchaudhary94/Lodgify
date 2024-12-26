b193f43c6ab1cd5eb00991ab3b18011807100f3f
saket singh <saketchaudhary2004@gmail.com>
2024-12-26 10:53:55 +0530

    Fix bug: problem of render  home page

--- routes/user.js
+++ routes/user.js
@@ -7,6 +7,9 @@ const { saveRedirectUrl } = require("../middleware.js");
 
 const userController = require("../controllers/users.js");
 
+router.route("/")
+.get(userController.renderHomePage);
+
 router.route("/signup")
 .get(userController.renderSignupForm)
 .post(wrapAsync(userController.signup));
